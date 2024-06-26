9dcb93a847e87a01fa86ebfb775482e9727aa0fd
Ammar Dalel <ammardalel07@gmail.com>
2024-06-26 11:31:07 +0100

    client creation and socket.io-client configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ app.use(router);
 // Configurer `socket.io` avec les types appropriés
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "http://localhost:5173",
 		methods: ["GET", "POST"]
 	}
 });
